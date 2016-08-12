c924f135fb65b180ba85bee8c5f59d9f7236cd19
Foysal Ahmed <foysal94@outlook.com>
2016-08-12 18:23:33 +0100

    Added helper function that attempts to parse the data returned from making the OpenWheater API call. Not implemented it into the component/container yet

--- app/Utilities/OpenWeatherAPI.js
+++ app/Utilities/OpenWeatherAPI.js
@@ -5,10 +5,26 @@ const fiveDayAPICall = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=
 const currentWeather = ' http://api.openweathermap.org/data/2.5/weather?q=CITY-NAME&type=accurate&APPID=YOUR-API-KEY '
 
 export function getFiveDayForecast(location){
-   return axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + location + '&type=accurate&APPID=' + apiKey + '&cnt=5')
+
 }
 
 export function getCurrentWeather(location) {
    return axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&type=accurate&APPID=' + apiKey)
 }
 
+const helpers = {
+   getFiveDayForecast: function(location) {
+      return axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + location + '&type=accurate&APPID=' + apiKey + '&cnt=5')
+   }
+   .then(function(response) {
+      return response.data.list.map(function (DayForecast) {
+         return [{
+            "DateTime" : DayForecast.dt,
+            "MinTemp" :DayForecast.main.temp_min,
+            "MaxTemp" : DayForecast.main.temp_max,
+            "Humidity" :DayForecast.main.humidity,
+            "Description" : DayForecast.weather[0].main
+         }]
+      })
+   })
+}
