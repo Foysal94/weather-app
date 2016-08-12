95cedb6064111781271077422276c3d801edfc53
Foysal Ahmed <foysal94@outlook.com>
2016-08-12 15:18:28 +0100

    Added axios to package.json. Added OpenWeather 5 day/3 hour forcast API calls as strings, and functions returning data from the API using axios

--- app/Utilities/OpenWeatherAPI.js
+++ app/Utilities/OpenWeatherAPI.js
@@ -1,6 +1,14 @@
-const apiKey = '2208d407ec5744e0b06648427dce293b'
-const currentWeatherAPICall = 'http://api.openweathermap.org/data/2.5/forecast/city?&APPID={APIKEY}'
+import axios from 'axios'
 
-function getCityWeather(location){
-   var data = http://api.openweathermap.org/data/2.5/forecast/city?&APPID={APIKEY}
+const apiKey = '2208d407ec5744e0b06648427dce293b'
+const fiveDayAPICall = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=CITY-NAME&type=accurate&APPID=YOUR-API-KEY&cnt=5';
+const currentWeather = ' http://api.openweathermap.org/data/2.5/weather?q=CITY-NAME&type=accurate&APPID=YOUR-API-KEY '
+
+export function getFiveDayForecast(location){
+   return axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + location + '&type=accurate&APPID=' + apiKey)
 }
+
+export function getCurrentWeather(location) {
+   return axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&type=accurate&APPID=' + apiKey)
+}
+
