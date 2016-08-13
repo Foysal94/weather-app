320e14767043fdcce1c5aa64cbdf6eb90a3f0284
Foysal Ahmed <foysal94@outlook.com>
2016-08-13 22:40:00 +0100

    Added convertNumberToDay function accepting a number corresponding to a day and returns the correct day

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -5,6 +5,50 @@ function puke(object) {
    //<div>DATA: {puke(props)}</div>
 }
 
+function convertDayNumberToString(dayNumber) {
+   var day;
+   switch(dayNumber)
+   {
+      case 0:
+         day = "Sunday"
+         break;
+
+      case 1:
+         day = "Monday"
+         break;
+
+      case 2:
+         day = "Tuesday"
+         break;
+
+      case 3:
+         day = "Wednesday"
+         break;
+
+      case 4:
+         day = "Thursday"
+         break;
+
+      case 5:
+         day = "Friday"
+         break;
+
+      case 6:
+         day = "Saturday"
+         break;
+
+      case 7:
+         day = "Sunday"
+         break;
+
+      default:
+         day = "ERROR"
+         break;
+   }
+
+   return day;
+}
+
 
 function Forecast(props) {
    return (
@@ -14,12 +58,14 @@ function Forecast(props) {
           <ul>
              {
                 props.fiveDayForecast.map( function(dayForecast) {
-                  return(
-                      <li>
-                         <pre>{JSON.stringify(dayForecast,null, ' ')}</pre>
-                      </li>
-                  )
-               })
+                   return(
+                       <li>
+                         <pre>
+         
+                         </pre>
+                       </li>
+                   )
+                })
              }
           </ul>
        </div>
