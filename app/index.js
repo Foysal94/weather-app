6a35b0ec31d396620bbb1235baeee54c2c235206
Foysal Ahmed <foysal94@outlook.com>
2016-08-14 14:13:10 +0100

    Moved the Main.scss inside the scss folder. Imported it inside index.js so webpack bundles all the scss modules inside one

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './config/routes';
+import MainStyles from './scss/Main.scss'
 
 ReactDOM.render(routes, document.getElementById('root'));
 
