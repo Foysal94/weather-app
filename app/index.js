fd6656ebef843a8df0d8ebccb6995394c5a0e9b4
Foysal Ahmed <foysal94@outlook.com>
2016-08-15 00:32:59 +0100

    Added weather-icons required files into the scss folder. Imported it inside index.js. Removed old code from Main.css. Webpack handles all the css fine

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './config/routes';
-import MainStyles from './scss/Main.scss'
+import MainStyles from './scss/Main.css'
+import weatherIcons from './scss/weather-icons/css/weather-icons.css'
 
 ReactDOM.render(routes, document.getElementById('root'));
 
