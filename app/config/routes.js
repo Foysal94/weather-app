ad98b43d13bde1c6613929aad11c4b857da8d6a6
Foysal Ahmed <foysal94@outlook.com>
2016-08-19 20:34:30 +0100

    Added a 'qs' package to stringify/parse objects/arrays. Google it for more detail. Designed a custom history object using 'qs' per Google help. Not implemented however

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,6 +4,17 @@ import Main from '../components/Main';
 import Home from '../components/Home';
 import ForecastContainer from '../containers/ForecastContainer'
 
+import { stringify, parse } from 'qs'
+import { useRouterHistory } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+
+const stringifyQuery = function(query) {
+  stringify(query, { arrayFormat: 'brackets' })
+}
+const history = useRouterHistory(createBrowserHistory) ({
+  parseQueryString: parse, stringifyQuery
+})
+
 var routes = (
     <Router history={hashHistory}>
         <Route path='/' component={Main}>
