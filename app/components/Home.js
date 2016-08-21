b2f89c5f79b35a4a20a37ac75928be67d578a46b
Foysal Ahmed <foysal94@outlook.com>
2016-08-21 23:22:34 +0100

    Renamed SearchBox element

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {default as SearchBox} from '../containers/LocationSearchBoxContainer'
+import {default as SearchBoxContainer} from '../containers/LocationSearchBoxContainer'
 import styles from '../styles/components/Home.css'
 
 export default function Home() {
    return (
        <div className={styles.center}>
           <h2>Enter a city and state</h2>
-          <SearchBox />
+          <SearchBoxContainer />
        </div>
    )
 }
