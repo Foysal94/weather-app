00947730b6c9816802dbbf68b0427bdac8aa55e6
Foysal Ahmed <foysal94@outlook.com>
2016-08-22 19:21:10 +0100

    Changed Main.js component from a React ES6 class to a stateless function

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,23 +1,15 @@
 import React, {Component} from 'react';
 import MainNavbar from './MainNavbar'
 
-
-class Main extends Component{
-
-   constructor(props) {
-      super(props);
-      this.props = props;
-   }
-
-   render() {
-      return(
-          <div>
-             <MainNavbar/>
-             {this.props.children}
-          </div>
-      )
-   }
+function Main(props) {
+  return(
+      <div>
+        <MainNavbar/>
+        {props.children}
+      </div>
+  )
 
 }
+
 
 export default Main
